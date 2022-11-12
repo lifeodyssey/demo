11156073906e38c5e31e5ef7fea636aa13066a3f
zhenjia.zhou <zhenjia.zhou@thoughtworks.com>
2022-11-12 18:52:29 +0800

    Initial commit

---
+++ src/main/kotlin/com/example/demo/Controller/HelloController.kt
@@ -0,0 +1,14 @@
+package com.example.demo
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class HelloController {
+    @RequestMapping("/")
+    @ResponseBody
+    fun getHello(): String {
+        return "Hello"
+    }
+}
---
+++ src/test/kotlin/com/example/demo/HelloControllerTest.kt
@@ -0,0 +1,12 @@
+package com.example.demo
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class HelloControllerTest {
+
+    @Test
+    fun getHello() {
+    }
+}
