8130135d7782218e4c12465a5beaec05d5a7d7ff
zhenjia.zhou <zhenjia.zhou@thoughtworks.com>
2022-11-21 00:44:56 +0800

    chore: add response status to the getHello API

--- src/main/kotlin/com/example/demo/controller/HelloController.kt
+++ src/main/kotlin/com/example/demo/controller/HelloController.kt
@@ -1,13 +1,16 @@
 package com.example.demo.controller
 
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @Controller
 class HelloController {
     @RequestMapping("/")
     @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
     fun getHello(): String {
         return "Hello"
     }
