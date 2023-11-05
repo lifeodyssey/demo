ad5811baefa196c29ea7c7ab7131091d0a3b30e7
lifeodyssey <zhouthepassion@outlook.com>
2023-11-05 21:28:57 +0800

    feat: remove keycloack and add basic auth

---
+++ bff/src/main/kotlin/com/example/bff/config/BasicAuthConfiguration.kt
@@ -0,0 +1,43 @@
+package com.example.bff.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+class BasicAuthConfiguration {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain  {
+        http.csrf { it.disable() }
+            .httpBasic(Customizer.withDefaults())
+            .authorizeHttpRequests {
+            it.anyRequest().authenticated()
+            }
+        return http.build()
+    }
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val gxp = User.withUsername("gxp")
+            .password(passwordEncoder().encode("gxp"))
+            .roles("USER")
+            .build()
+        val admin = User.withUsername("admin")
+            .password(passwordEncoder().encode("admin"))
+            .roles("ADMIN")
+            .build()
+        return InMemoryUserDetailsManager(gxp, admin)
+    }
+}
